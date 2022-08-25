686d70a50da4987d0fd4af71c47c417a9b453805
ghboo <ghboo0927@gmail.com>
2022-08-25 13:20:49 +0900

    update

--- auth/auth.go
+++ auth/auth.go
@@ -2,16 +2,25 @@ package auth
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/zmb3/spotify"
 )
 
 var redirectURL = "http://127.0.0.1:8080/callback"
-var ClientID = "70d3fbedecc646b184fb5407783140dd"
-var ClientSecret = "9781d25cd84041ae802c764d0694b6a7"
+
+// var ClientID = os.Getenv("CLIENT_ID")
+// var ClientSecret = os.Getenv("CLIENT_SECERET")
 var auth = spotify.NewAuthenticator(redirectURL, spotify.ScopePlaylistReadCollaborative, spotify.ScopePlaylistModifyPublic)
 
 func MakeAuthURL() string {
+	err := godotenv.Load()
+	if err != nil {
+		//send error
+	}
+	var ClientID = os.Getenv("CLIENT_ID")
+	var ClientSecret = os.Getenv("CLIENT_SECERET")
 	auth.SetAuthInfo(ClientID, ClientSecret)
 	url := auth.AuthURL("state")
 	return url
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/zmb3/spotify v1.3.0
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/oauth2 v0.0.0-20220808172628-8227340efae7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
